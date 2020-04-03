81a1bc7f2571986e49113085dc839a330fb28e7c
Dominik Wojciechowski <wojdom29@gmail.com>
2020-04-03 13:31:57 +0200

    implemented fetching todos from an array; added event listener to the button

--- js/todo_list.js
+++ js/todo_list.js
@@ -5,18 +5,27 @@ function TodoList(containerId) {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit 2",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit 3",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit 4",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 5"
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 5",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 6",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 7",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 8"
     ];
     this.createList = function () {
         var todoContainer = document.getElementById(containerId);
+        var buttonAdd = todoContainer.children[0].children[0];
         var fragmentContainer = new DocumentFragment();
-        var wrapper = document.createElement("div");
-        var paragraph = document.createElement("p");
-        paragraph.textContent = "new item";
-        wrapper.classList.add("todo-item");
-        wrapper.appendChild(paragraph);
-        fragmentContainer.appendChild(wrapper);
+        this.todoList.forEach(function (item) {
+            var wrapper = document.createElement("div");
+            wrapper.classList.add("todo-item");
+            var paragraph = document.createElement("p");
+            paragraph.textContent = item;
+            wrapper.appendChild(paragraph);
+            fragmentContainer.appendChild(wrapper);
+        });
         todoContainer.appendChild(fragmentContainer);
+        buttonAdd.addEventListener("click", function () {
+            console.log("clicked on the add button");
+        });
     };
     // debug
     this.getList = function () {
--- ts/todo_list.ts
+++ ts/todo_list.ts
@@ -5,19 +5,31 @@ function TodoList(containerId: string) {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit 2",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit 3",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit 4",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 5"
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 5",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 6",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 7",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 8"
   ];
 
   this.createList = function() {
     const todoContainer = document.getElementById(containerId);
+    const buttonAdd = todoContainer.children[0].children[0];
     let fragmentContainer = new DocumentFragment();
-    let wrapper = document.createElement("div");
-    let paragraph = document.createElement("p");
-    paragraph.textContent = "new item";
-    wrapper.classList.add("todo-item");
-    wrapper.appendChild(paragraph);
-    fragmentContainer.appendChild(wrapper);
+
+    this.todoList.forEach(item => {
+      let wrapper = document.createElement("div");
+      wrapper.classList.add("todo-item");
+      let paragraph = document.createElement("p");
+      paragraph.textContent = item;
+      wrapper.appendChild(paragraph);
+      fragmentContainer.appendChild(wrapper);
+    });
+
     todoContainer.appendChild(fragmentContainer);
+
+    buttonAdd.addEventListener("click", () => {
+      console.log("clicked on the add button");
+    });
   };
 
   // debug
