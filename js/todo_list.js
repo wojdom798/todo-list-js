10dd7673eabac01d5844f771f8eb273a74c79430
Dominik Wojciechowski <wojdom29@gmail.com>
2020-04-03 16:46:58 +0200

    implemented modal form for adding a todo item

--- js/todo_list.js
+++ js/todo_list.js
@@ -1,18 +1,10 @@
 function TodoList(containerId) {
-    this.todoItem = null;
-    this.todoList = [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 1",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 2",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 3",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 4",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 5",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 6",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 7",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit 8"
-    ];
+    this.todoContainer = null;
+    this.buttonAdd = null;
+    this.todoList = [];
+    this.modal = null;
+    this.isModalOpen = false;
     this.createList = function () {
-        var todoContainer = document.getElementById(containerId);
-        var buttonAdd = todoContainer.children[0].children[0];
         var fragmentContainer = new DocumentFragment();
         this.todoList.forEach(function (item) {
             var wrapper = document.createElement("div");
@@ -22,10 +14,63 @@ function TodoList(containerId) {
             wrapper.appendChild(paragraph);
             fragmentContainer.appendChild(wrapper);
         });
-        todoContainer.appendChild(fragmentContainer);
-        buttonAdd.addEventListener("click", function () {
-            console.log("clicked on the add button");
+        this.todoContainer.appendChild(fragmentContainer);
+    };
+    this.init = function () {
+        var _this = this;
+        this.todoContainer = document.getElementById(containerId);
+        this.buttonAdd = this.todoContainer.children[0].children[0];
+        if (!this.todoContainer.classList.contains("todo-container")) {
+            this.todoContainer.classList.add("todo-container");
+        }
+        this.createModal();
+        this.buttonAdd.addEventListener("click", function () {
+            _this.addTodo();
         });
+    };
+    this.addTodo = function () {
+        if (!this.isModalOpen) {
+            this.modal.classList.add("active");
+            this.isModalOpen = true;
+        }
+    };
+    this.deleteTodo = function (index) {
+    };
+    this.closeModal = function () {
+        this.isModalOpen = false;
+        this.modal.classList.remove("active");
+    };
+    this.createModal = function () {
+        this.modal = document.createElement("div");
+        this.modal.classList.add("modal");
+        var modalContents = document.createElement("div");
+        modalContents.classList.add("add-todo-container");
+        var header = document.createElement("h3");
+        var form = document.createElement("form");
+        var txtInput = document.createElement("input");
+        txtInput.setAttribute("type", "text");
+        txtInput.setAttribute("name", "todo-input");
+        var wrapper = document.createElement("div");
+        var saveBtn = document.createElement("button");
+        saveBtn.setAttribute("type", "button");
+        saveBtn.setAttribute("name", "save-btn");
+        saveBtn.textContent = "save";
+        var closeBtn = document.createElement("button");
+        closeBtn.setAttribute("type", "button");
+        closeBtn.setAttribute("name", "close-btn");
+        closeBtn.textContent = "close";
+        // if you don't bind 'this' to closeModal()
+        // 'this' will refer to button inside the event handler
+        closeBtn.addEventListener("click", this.closeModal.bind(this));
+        wrapper.appendChild(saveBtn);
+        wrapper.appendChild(closeBtn);
+        form.appendChild(txtInput);
+        form.appendChild(wrapper);
+        form.appendChild(wrapper);
+        modalContents.appendChild(header);
+        modalContents.appendChild(form);
+        this.modal.appendChild(modalContents);
+        document.body.appendChild(this.modal);
     };
     // debug
     this.getList = function () {
@@ -33,4 +78,5 @@ function TodoList(containerId) {
     };
 }
 var todoList = new TodoList("todolist");
+todoList.init();
 todoList.createList();
--- ts/todo_list.ts
+++ ts/todo_list.ts
@@ -1,19 +1,11 @@
 function TodoList(containerId: string) {
-  this.todoItem = null;
-  this.todoList = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 1",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 2",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 3",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 4",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 5",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 6",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 7",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit 8"
-  ];
+  this.todoContainer = null;
+  this.buttonAdd = null;
+  this.todoList = [];
+  this.modal = null;
+  this.isModalOpen = false;
 
   this.createList = function() {
-    const todoContainer = document.getElementById(containerId);
-    const buttonAdd = todoContainer.children[0].children[0];
     let fragmentContainer = new DocumentFragment();
 
     this.todoList.forEach(item => {
@@ -25,11 +17,81 @@ function TodoList(containerId: string) {
       fragmentContainer.appendChild(wrapper);
     });
 
-    todoContainer.appendChild(fragmentContainer);
+    this.todoContainer.appendChild(fragmentContainer);
 
-    buttonAdd.addEventListener("click", () => {
-      console.log("clicked on the add button");
+
+  };
+
+  this.init = function() {
+    this.todoContainer = document.getElementById(containerId);
+    this.buttonAdd = this.todoContainer.children[0].children[0];
+    if (!this.todoContainer.classList.contains("todo-container")) {
+      this.todoContainer.classList.add("todo-container");
+    }
+
+    this.createModal();
+
+    this.buttonAdd.addEventListener("click", () => {
+      this.addTodo();
     });
+  };
+
+  this.addTodo = function() {
+    if (!this.isModalOpen) {
+      this.modal.classList.add("active");
+      this.isModalOpen = true;
+    }
+  };
+
+  this.deleteTodo = function(index) {
+
+  };
+
+  this.closeModal = function() {
+    this.isModalOpen = false;
+    this.modal.classList.remove("active");
+  };
+
+  this.createModal = function() {
+    this.modal = document.createElement("div");
+    this.modal.classList.add("modal");
+
+    let modalContents = document.createElement("div");
+    modalContents.classList.add("add-todo-container");
+
+    let header = document.createElement("h3");
+    let form = document.createElement("form");
+
+    let txtInput = document.createElement("input");
+    txtInput.setAttribute("type", "text");
+    txtInput.setAttribute("name", "todo-input");
+
+    let wrapper =  document.createElement("div");
+
+    let saveBtn = document.createElement("button");
+    saveBtn.setAttribute("type", "button");
+    saveBtn.setAttribute("name", "save-btn");
+    saveBtn.textContent = "save";
+
+    let closeBtn = document.createElement("button");
+    closeBtn.setAttribute("type", "button");
+    closeBtn.setAttribute("name", "close-btn");
+    closeBtn.textContent = "close";
+
+    // if you don't bind 'this' to closeModal()
+    // 'this' will refer to button inside the event handler
+    closeBtn.addEventListener("click", this.closeModal.bind(this));
+
+    wrapper.appendChild(saveBtn);
+    wrapper.appendChild(closeBtn);
+    form.appendChild(txtInput);
+    form.appendChild(wrapper);
+    form.appendChild(wrapper);
+    modalContents.appendChild(header);
+    modalContents.appendChild(form);
+    this.modal.appendChild(modalContents);
+
+    document.body.appendChild(this.modal);
   };
 
   // debug
@@ -39,4 +101,5 @@ function TodoList(containerId: string) {
 }
 
 const todoList = new TodoList("todolist");
+todoList.init();
 todoList.createList();
